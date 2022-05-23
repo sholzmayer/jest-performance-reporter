d25a28594808ab8f4cf4077806dadd8dea125c0c
Simon Holzmayer <simon@holzmayer.me>
2022-05-23 10:50:40 +0200

    fix: avoid infinite runs on watch mode
    as file reports retriggered test execution

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,24 +7,32 @@ import { LogLevelOptions } from './reports/terminal/log';
 
 const setupAndRun = async (data: {
   testData: AggregatedResult;
-  options: { jsonReportPath?: string; csvReportPath?: string } & LogLevelOptions;
+  options: {
+    jsonReportPath?: string;
+    csvReportPath?: string;
+  } & LogLevelOptions;
+  skipFileReport: boolean;
 }) => {
-  const { testData, options } = data;
+  const { testData, options, skipFileReport } = data;
   const { warnAfterMs, errorAfterMs, logLevel, maxItems, jsonReportPath, csvReportPath } = options;
   const reports = extractJestReports(testData);
 
   printTestResults(reports, { errorAfterMs, warnAfterMs, logLevel, maxItems });
-  jsonReportPath && saveJsonReport(reports, jsonReportPath);
-  csvReportPath && (await saveCsvReport(reports, csvReportPath));
+  !skipFileReport && jsonReportPath && saveJsonReport(reports, jsonReportPath);
+  !skipFileReport && csvReportPath && (await saveCsvReport(reports, csvReportPath));
 };
 
 export default class JestPerformanceReporter implements Reporter {
-  constructor(_: any, public reporterOptions: any) {}
+  constructor(
+    public jestOptions: { watch: boolean; watchAll: boolean },
+    public reporterOptions: any
+  ) {}
 
   onRunComplete(testContexts: Set<TestContext>, results: AggregatedResult) {
     setupAndRun({
       testData: results,
       options: this.reporterOptions,
+      skipFileReport: this.jestOptions.watch || this.jestOptions.watchAll,
     });
   }
 
